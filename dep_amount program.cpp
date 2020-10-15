f684b8fda343e8507c2013f05293a03abeec667b
myrdall <trivarson@gmail.com>
2020-10-15 12:28:29 +0700

    First Conditions for 0-30 days

--- dep_amount program.cpp
+++ dep_amount program.cpp
@@ -6,8 +6,35 @@
  {
       setlocale(LC_ALL, "Russian");
 
-     int days, income, dep_amount;
- 
+     int days, income, dep_amount, percent, percent_amount;
+
+     cout << "Введите сумму вклада ";
+     cin >>dep_amount; 
+
+     cout << "Введите срок вклада ";
+     cin >>days;
+
+     if (days <= 30){
+
+         percent = 10;
+
+         percent_amount = dep_amount * percent / 100;
+
+         income = dep_amount - percent_amount;
+
+         cout <<"Cумма вклада на окончание срока: "<< income << "руб.";
+     } 
+
+     if (dep_amount < 1000)
+     {
+        cout << "Минимальная сумма вклада состовляет 1000 рублей";
+     }
+     if (days > 365)
+     {
+        cout << "Срок вклада не может быть более 365 дней";
+     }
+     
+    
  
  }
  
