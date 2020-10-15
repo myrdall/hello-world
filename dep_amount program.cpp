84972fb02cdd217dad174641e42cd12eaa60c4fb
myrdall <trivarson@gmail.com>
2020-10-15 12:45:19 +0700

    Correcting input conditions and new condition for 31 - 120 days

--- dep_amount program.cpp
+++ dep_amount program.cpp
@@ -11,8 +11,22 @@
      cout << "Введите сумму вклада ";
      cin >>dep_amount; 
 
+    while (dep_amount < 1000)
+    {   
+        cout << "Минимальная сумма вклада состовляет 1000 рублей \n";
+        cout << "Введите сумму вклада ";
+        cin >>dep_amount;     
+
+    }
+    
      cout << "Введите срок вклада ";
      cin >>days;
+
+     if (days > 365)
+     {
+        cout << "Срок вклада не может быть более 365 дней";
+     }
+
 
      if (days <= 30){
 
@@ -22,20 +36,23 @@
 
          income = dep_amount - percent_amount;
 
-         cout <<"Cумма вклада на окончание срока: "<< income << "руб.";
+         
      } 
 
-     if (dep_amount < 1000)
-     {
-        cout << "Минимальная сумма вклада состовляет 1000 рублей";
-     }
-     if (days > 365)
-     {
-        cout << "Срок вклада не может быть более 365 дней";
-     }
-     
+    if (days > 30 && days <= 120 ){
+
+         percent = 2;
+
+         percent_amount = dep_amount * percent / 100;
+
+         income = dep_amount + percent_amount;
+    }
+
     
- 
+     
+
+     cout <<"Cумма вклада на окончание срока: "<< income << "руб.";
+
  }
  
  
